4ba3d12fae593c9fbaec2ca94b3aafd6a6825f59
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-02-28 23:29:20 -0300

    1.2
    Adicionamos a interface, criamos as rotas das páginas, adicionamos funcionalidades aos botões, atualizamos as fontes e mais outras pequenos ajustes

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgStyle } from "@angular/common";
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, NgStyle],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
 export class CertificadoForm {
 
+  atividade: string = '';
+  certificado: Certificado = {
+    atividades: [],
+    nome: ''
+  }
+
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched;
+  }
+
+  formValido(){
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
+  }
+
+  adicionarAtividade(){
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number){
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit(){
+    if(!this.formValido){
+      return;
+    }
+  }
 }
---
+++ src/app/pages/certificado/certificado.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../_components/navbar/navbar";
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-certificado',
+  imports: [SecondaryButton, RouterLink],
+  templateUrl: './certificado.html',
+  styleUrl: './certificado.css',
+})
+export class Certificado {
+
+}
